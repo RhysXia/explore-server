0386fbf2a3e86ca259296c10baa819f90ca0b932
RhysXia <xrs4433@outlook.com>
2021-05-12 09:17:19 +0800

    update

--- server/src/main/kotlin/me/rhysxia/explore/server/ServerApplication.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/ServerApplication.kt
@@ -1,9 +1,8 @@
 package me.rhysxia.explore.server
 
 import graphql.GraphQL
-import graphql.Scalars.GraphQLString
-import graphql.language.FieldDefinition.newFieldDefinition
-import graphql.schema.GraphQLObjectType
+import graphql.Scalars
+import graphql.schema.GraphQLFieldDefinition
 import graphql.schema.GraphQLObjectType.newObject
 import graphql.schema.GraphQLSchema
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -13,21 +12,26 @@ import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class ServerApplication {
-  @Bean
-  fun graphQL(): GraphQL {
-    val o = newObject()
-      .name("user")
-      .field(newFieldDefinition()
-          .name("username")
-          .type(GraphQLString))
-      .build()
+    @Bean
+    fun graphQL(): GraphQL {
+        val o = newObject()
+            .name("user")
+            .field(
+                GraphQLFieldDefinition.newFieldDefinition()
+                    .name("username")
+                    .type(Scalars.GraphQLString)
+            )
+            .build()
 
+        val schema = GraphQLSchema
+            .newSchema()
+            .additionalType(o)
+            .build()
 
-
-    return GraphQL.newGraphQL(schema).build()
-  }
+        return GraphQL.newGraphQL(schema).build()
+    }
 }
 
 fun main(args: Array<String>) {
-  runApplication<ServerApplication>(*args)
+    runApplication<ServerApplication>(*args)
 }
