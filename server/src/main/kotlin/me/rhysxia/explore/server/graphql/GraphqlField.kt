44e3e0692b05900e9c45b9d6191334a102cf4799
Rhys Xia <xrs4433@outlook.com>
2021-05-13 23:55:31 +0800

    init

--- server/src/main/kotlin/me/rhysxia/explore/server/graphql/GraphqlField.kt
+++
@@ -1,6 +0,0 @@
-package me.rhysxia.explore.server.graphql
-
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-@Target(AnnotationTarget.FUNCTION)
-annotation class GraphqlField(val name: String = "")
