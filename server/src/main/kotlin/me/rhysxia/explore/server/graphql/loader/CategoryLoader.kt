7503af3eb94f03581e4f3e648e63dde98576a9b2
Rhys Xia <xrs4433@outlook.com>
2021-05-17 22:58:26 +0800

    fetch 支持suspend和flow

---
+++ server/src/main/kotlin/me/rhysxia/explore/server/graphql/loader/CategoryLoader.kt
@@ -0,0 +1,15 @@
+package me.rhysxia.explore.server.graphql.loader
+
+import me.rhysxia.explore.server.configuration.graphql.annotation.GraphqlLoader
+import me.rhysxia.explore.server.po.CategoryPo
+import org.dataloader.BatchLoader
+import java.time.Instant
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
+
+@GraphqlLoader("category")
+class CategoryLoader : BatchLoader<Long, CategoryPo> {
+  override fun load(keys: MutableList<Long>): CompletionStage<MutableList<CategoryPo>> {
+    return CompletableFuture.completedStage(mutableListOf(CategoryPo(1, "aa", "aaa", Instant.now(), Instant.now())))
+  }
+}
