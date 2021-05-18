2389299c5e2da87a28d9db4a4f3b3f8657bdb73b
Rhys Xia <xrs4433@outlook.com>
2021-05-18 23:15:38 +0800

    集成分页

--- server/src/main/kotlin/me/rhysxia/explore/server/configuration/graphql/GraphqlController.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/configuration/graphql/GraphqlController.kt
@@ -12,34 +12,29 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
-data class GraphqlRequestBody(
-    val query: String?,
-    val variables: Map<String, Any> = emptyMap(),
-    val operationName: String?
-)
 
 @RestController
 class GraphqlController(private val graphql: GraphQL, private val loaderMap: Map<String, BatchLoader<*, *>>) {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
+  private val logger = LoggerFactory.getLogger(this.javaClass)
 
 
-    @PostMapping("/graphql")
-    fun graphql(@RequestBody graphqlRequestBody: GraphqlRequestBody): Mono<MutableMap<String, Any>> {
-        val dataLoaderRegister = DataLoaderRegistry()
+  @PostMapping("/graphql")
+  fun graphql(@RequestBody graphqlRequestBody: GraphqlRequestBody): Mono<MutableMap<String, Any>> {
+    val dataLoaderRegister = DataLoaderRegistry()
 
-        this.loaderMap.forEach {
-            dataLoaderRegister.register(it.key, DataLoader.newDataLoader(it.value))
-        }
-
-        val executionInput = ExecutionInput.newExecutionInput()
-            .query(graphqlRequestBody.query)
-            .variables(graphqlRequestBody.variables)
-            .operationName(graphqlRequestBody.operationName)
-            .dataLoaderRegistry(dataLoaderRegister)
-            .build()
-
-        return graphql.executeAsync(executionInput).toMono().map { it.toSpecification() }
-
+    this.loaderMap.forEach {
+      dataLoaderRegister.register(it.key, DataLoader.newDataLoader(it.value))
     }
+
+    val executionInput = ExecutionInput.newExecutionInput()
+      .query(graphqlRequestBody.query)
+      .variables(graphqlRequestBody.variables)
+      .operationName(graphqlRequestBody.operationName)
+      .dataLoaderRegistry(dataLoaderRegister)
+      .build()
+
+    return graphql.executeAsync(executionInput).toMono().map { it.toSpecification() }
+
+  }
 }
