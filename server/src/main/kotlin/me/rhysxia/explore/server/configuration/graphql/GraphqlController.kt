08e87009f6d45eaccde225dcb47357c3243c9b84
RhysXia <xrs4433@outlook.com>
2021-05-18 19:09:09 +0800

    update

--- server/src/main/kotlin/me/rhysxia/explore/server/configuration/graphql/GraphqlController.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/configuration/graphql/GraphqlController.kt
@@ -2,7 +2,6 @@ package me.rhysxia.explore.server.configuration.graphql
 
 import graphql.ExecutionInput
 import graphql.GraphQL
-import me.rhysxia.explore.server.configuration.graphql.annotation.GraphqlLoader
 import org.dataloader.BatchLoader
 import org.dataloader.DataLoader
 import org.dataloader.DataLoaderRegistry
@@ -20,43 +19,16 @@ data class GraphqlRequestBody(
 )
 
 @RestController
-class GraphqlController(private val graphql: GraphQL, loaders: List<BatchLoader<*, *>>) {
+class GraphqlController(private val graphql: GraphQL, private val loaderMap: Map<String, BatchLoader<*, *>>) {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    private val loaders: Map<String, BatchLoader<*, *>>
-
-    init {
-        this.loaders = loaders.mapNotNull {
-            val graphqlLoader = it.javaClass.getAnnotation(GraphqlLoader::class.java)
-            if (graphqlLoader === null) {
-                logger.debug(
-                    "BatchLoader '%s' should has annotation '%s'.",
-                    it.javaClass.canonicalName,
-                    GraphqlLoader::class.java
-                )
-                return@mapNotNull null
-            }
-            val name = graphqlLoader.name
-            if (name.isBlank()) {
-                logger.debug(
-                    "BatchLoader '%s' should has a name, but be blank.",
-                    it.javaClass.canonicalName
-                )
-                return@mapNotNull null
-            }
-            name to it
-        }.fold(HashMap<String, BatchLoader<*, *>>()) { a, b ->
-            a[b.first] = b.second
-            a
-        }
-    }
 
     @PostMapping("/graphql")
     fun graphql(@RequestBody graphqlRequestBody: GraphqlRequestBody): Mono<MutableMap<String, Any>> {
         val dataLoaderRegister = DataLoaderRegistry()
 
-        this.loaders.forEach {
+        this.loaderMap.forEach {
             dataLoaderRegister.register(it.key, DataLoader.newDataLoader(it.value))
         }
 
