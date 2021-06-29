8bef3c26b96e6b8f915365286c024bf80f5b9eb7
RhysXia <xrs4433@outlook.com>
2021-06-29 14:06:22 +0800

    login

--- src/main/kotlin/me/rhysxia/explore/server/repository/UserRepository.kt
+++ src/main/kotlin/me/rhysxia/explore/server/repository/UserRepository.kt
@@ -4,5 +4,5 @@ import me.rhysxia.explore.server.po.UserPo
 import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 
 interface UserRepository : CoroutineSortingRepository<UserPo, Long> {
-
+  suspend fun findByUsername(username: String): UserPo?
 }
--- src/main/kotlin/me/rhysxia/explore/server/service/impl/TokenServiceImpl.kt
+++ src/main/kotlin/me/rhysxia/explore/server/service/impl/TokenServiceImpl.kt
@@ -1,36 +1,59 @@
 package me.rhysxia.explore.server.service.impl
 
 import me.rhysxia.explore.server.dto.AuthUser
+import me.rhysxia.explore.server.exception.ParameterException
+import me.rhysxia.explore.server.po.TokenPo
 import me.rhysxia.explore.server.po.UserStatus
 import me.rhysxia.explore.server.repository.TokenRepository
 import me.rhysxia.explore.server.repository.UserRepository
 import me.rhysxia.explore.server.service.TokenService
+import me.rhysxia.explore.server.utils.PasswordUtils
+import me.rhysxia.explore.server.utils.TokenUtils
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
 @Service
-class TokenServiceImpl(private val tokenRepository: TokenRepository, private val userRepository: UserRepository) : TokenService {
+class TokenServiceImpl(private val tokenRepository: TokenRepository, private val userRepository: UserRepository) :
+  TokenService {
 
-    @Transactional
-    override suspend fun findAuthUserByToken(token: String): AuthUser? {
-        val tokenPo = tokenRepository.findById(token) ?: return null
+  @Transactional
+  override suspend fun findAuthUserByToken(token: String): AuthUser? {
+    val tokenPo = tokenRepository.findById(token) ?: return null
 
-        val now = Instant.now()
+    val now = Instant.now()
 
-        val userPo = userRepository.findById(tokenPo.userId)
+    val userPo = userRepository.findById(tokenPo.userId)
 
-        if (userPo == null || userPo.status != UserStatus.ACTIVATED || now.epochSecond - tokenPo.updatedAt.epochSecond > 1 * 60 * 60 * 24) {
-            // 删除token
-            tokenRepository.delete(tokenPo)
-            return null
-        }
-
-        val newToken = tokenPo.copy(updatedAt = now)
-
-        tokenRepository.save(newToken)
-
-        return AuthUser(newToken, userPo)
-
+    if (userPo == null || userPo.status != UserStatus.ACTIVATED || now.epochSecond - tokenPo.updatedAt.epochSecond > 1 * 60 * 60 * 24) {
+      // 删除token
+      tokenRepository.delete(tokenPo)
+      return null
     }
+
+    val newToken = tokenPo.copy(updatedAt = now)
+
+    tokenRepository.save(newToken)
+
+    return AuthUser(newToken, userPo)
+
+  }
+
+  @Transactional
+  override suspend fun login(username: String, password: String): String {
+    val user = userRepository.findByUsername(username) ?: throw ParameterException("用户名或者密码不正确")
+
+    if (!PasswordUtils.match(password, user.password)) {
+      throw ParameterException("用户名或者密码不正确")
+    }
+
+    val tokenId = TokenUtils.generateToken()
+
+    val date = Instant.now()
+
+    val token = TokenPo(tokenId, user.id!!, date, date)
+
+    tokenRepository.save(token)
+    return tokenId
+  }
 }
