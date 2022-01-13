d5d348210b9e7a318e69b6050c26952b19abb791
RhysXia <xrs4433@outlook.com>
2022-01-13 14:00:32 +0800

    feat: update

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,12 +2,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
   implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
   implementation("org.springframework.boot:spring-boot-starter-mail")
-  implementation("org.springframework.boot:spring-boot-starter-websocket")
   implementation(project(":spring-boot-starter-graphql"))
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
   runtimeOnly("org.postgresql:postgresql")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
-
 }
