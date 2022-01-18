f98b24a61527399a2748fdfc17a7a40aa88d514a
Rhys Xia <xrs4433@outlook.com>
2022-01-18 23:03:58 +0800

    feat: support exception

--- spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/exception/GraphqlTypeException.kt
+++ spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/exception/GraphqlTypeException.kt
@@ -1,5 +1,7 @@
 package me.rhysxia.explore.autoconfigure.graphql.exception
 
-open class GraphqlTypeException(message: String, cause: Throwable?) : GraphqlException(message, cause) {
+import graphql.GraphQLException
+
+open class GraphqlTypeException(message: String, cause: Throwable?) : GraphQLException(message, cause) {
   constructor(message: String) : this(message, null)
 }
