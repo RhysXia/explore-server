55878c63b2cefbc464fa4e03119f2f8185d90e87
xiaruisong <xiaruisong.kuaishou.com>
2022-01-20 20:50:53 +0800

    feat: 分页支持

---
+++ server/src/main/kotlin/me/rhysxia/explore/server/graphql/resolver/PageableDataFetcherParameterResolver.kt
@@ -0,0 +1,35 @@
+package me.rhysxia.explore.server.graphql.resolver
+
+import graphql.schema.DataFetchingEnvironment
+import me.rhysxia.explore.autoconfigure.graphql.interfaces.GraphqlDataFetcherParameterResolver
+import me.rhysxia.explore.server.dto.OffsetPage
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import kotlin.reflect.KParameter
+import kotlin.reflect.jvm.javaType
+
+@Component
+class PageableDataFetcherParameterResolver : GraphqlDataFetcherParameterResolver<Pageable> {
+  override fun support(parameter: KParameter): Boolean {
+    val javaType = parameter.type.javaType
+
+    if (javaType is Class<*>) {
+      return javaType.isAssignableFrom(Pageable::class.java)
+    }
+
+    return false
+  }
+
+  override fun resolve(dfe: DataFetchingEnvironment, parameter: KParameter): Mono<Pageable> {
+
+    val offset = dfe.getArgumentOrDefault("offset", 0L)
+    val limit = dfe.getArgumentOrDefault("limit", 10)
+    val sort = dfe.getArgumentOrDefault("sort", Sort.unsorted())
+
+    val pageable = OffsetPage(offset, limit, sort)
+
+    return Mono.just(pageable)
+  }
+}
