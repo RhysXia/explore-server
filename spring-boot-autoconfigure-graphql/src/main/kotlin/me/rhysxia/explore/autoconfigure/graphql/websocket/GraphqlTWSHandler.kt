cca4189016ddcc040a7dade67a092e716aba1743
Rhys Xia <xrs4433@outlook.com>
2022-01-23 18:44:51 +0800

    feat: 封装请求上下文

--- spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/websocket/GraphqlTWSHandler.kt
+++ spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/websocket/GraphqlTWSHandler.kt
@@ -1,28 +1,27 @@
 package me.rhysxia.explore.autoconfigure.graphql.websocket
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import me.rhysxia.explore.autoconfigure.graphql.GraphqlConfigurationProperties
 import me.rhysxia.explore.autoconfigure.graphql.GraphqlExecutionProcessor
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
 
 class GraphqlTWSHandler(
-  private val objectMapper: ObjectMapper,
-  private val graphqlExecutionProcessor: GraphqlExecutionProcessor,
-  private val graphqlConfigurationProperties: GraphqlConfigurationProperties
+  objectMapper: ObjectMapper,
+  graphqlExecutionProcessor: GraphqlExecutionProcessor,
 ) : WebSocketHandler {
 
-  private val graphqlTWSEnhancer = GraphqlWebsocketEnhancer.Builder()
-    .before("connection_init", "connection_ack")
-    .run("subscribe", "next")
-    .errorRespondCode("error")
-    .stopReceiveCode("stop")
-    .completeRespondCode("complete")
-    .build()
+  private val graphqlTWSEnhancer =
+    GraphqlWebsocketEnhancer.Builder(objectMapper, graphqlExecutionProcessor)
+      .before("connection_init", "connection_ack")
+      .run("subscribe", "next")
+      .errorRespondCode("error")
+      .stopReceiveCode("stop")
+      .completeRespondCode("complete")
+      .build()
 
   override fun handle(session: WebSocketSession): Mono<Void> {
-    return graphqlTWSEnhancer.execute(session, objectMapper, graphqlExecutionProcessor, graphqlConfigurationProperties)
+    return graphqlTWSEnhancer.execute(session)
   }
 
   override fun getSubProtocols(): List<String> {
--- spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/websocket/GraphqlWSHandler.kt
+++ spring-boot-autoconfigure-graphql/src/main/kotlin/me/rhysxia/explore/autoconfigure/graphql/websocket/GraphqlWSHandler.kt
@@ -1,30 +1,29 @@
 package me.rhysxia.explore.autoconfigure.graphql.websocket
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import me.rhysxia.explore.autoconfigure.graphql.GraphqlConfigurationProperties
 import me.rhysxia.explore.autoconfigure.graphql.GraphqlExecutionProcessor
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
 
 class GraphqlWSHandler(
-  private val objectMapper: ObjectMapper,
-  private val graphqlExecutionProcessor: GraphqlExecutionProcessor,
-  private val graphqlConfigurationProperties: GraphqlConfigurationProperties
+  objectMapper: ObjectMapper,
+  graphqlExecutionProcessor: GraphqlExecutionProcessor,
 ) : WebSocketHandler {
 
 
-  private val graphqlWSEnhancer = GraphqlWebsocketEnhancer.Builder()
-    .before("connection_init", "connection_ack")
-    .run("start", "data")
-    .errorRespondCode("error")
-    .stopReceiveCode("stop")
-    .completeRespondCode("complete")
-    .terminateReceiveCode("connection_terminate")
-    .build()
+  private val graphqlWSEnhancer =
+    GraphqlWebsocketEnhancer.Builder(objectMapper, graphqlExecutionProcessor)
+      .before("connection_init", "connection_ack")
+      .run("start", "data")
+      .errorRespondCode("error")
+      .stopReceiveCode("stop")
+      .completeRespondCode("complete")
+      .terminateReceiveCode("connection_terminate")
+      .build()
 
   override fun handle(session: WebSocketSession): Mono<Void> {
-    return graphqlWSEnhancer.execute(session, objectMapper, graphqlExecutionProcessor, graphqlConfigurationProperties)
+    return graphqlWSEnhancer.execute(session)
   }
 
   override fun getSubProtocols(): List<String> {
