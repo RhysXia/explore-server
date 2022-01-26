d69deecb997afdd32b928c9dcabd16dc5f861888
xiaruisong <xiaruisong.kuaishou.com>
2022-01-26 09:59:01 +0800

    feat: 数据库初始化

--- server/src/main/kotlin/me/rhysxia/explore/server/initializer/DatabaseInitializer.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/initializer/DatabaseInitializer.kt
@@ -1,35 +1,26 @@
 package me.rhysxia.explore.server.initializer
 
-import kotlinx.coroutines.runBlocking
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
-import org.springframework.r2dbc.core.DatabaseClient
-import org.springframework.r2dbc.core.awaitOne
-import org.springframework.stereotype.Component
-import org.springframework.util.ResourceUtils
-
-@Component
-class DatabaseInitializer(private val databaseClient: DatabaseClient) : ApplicationRunner {
-
-  /**
-   * 是否已经初始化过了
-   */
-  private suspend fun isInitialized(): Boolean {
-    val spec = databaseClient.sql("select count(1) from pg_class where relname = 'sys_user'").fetch()
-
-    val row = spec.awaitOne()
-
-    return (row["count"] as Long) > 0
-  }
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 
-  override fun run(args: ApplicationArguments) = runBlocking {
-    val schemaFile = ResourceUtils.getFile("classpath*:/schema.sql")
+@Configuration
+class DatabaseInitializer(
+) {
 
-    if (isInitialized()) {
-      return@runBlocking
-    }
-
-
+  @Bean
+  fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+    val initializer = ConnectionFactoryInitializer()
+    initializer.setConnectionFactory(connectionFactory)
+    val populator = CompositeDatabasePopulator()
+    populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+//    populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("data.sql")))
+    initializer.setDatabasePopulator(populator)
+    return initializer
   }
 }
--- server/src/test/kotlin/me/rhysxia/explore/server/ServerApplicationTest.kt
+++ server/src/test/kotlin/me/rhysxia/explore/server/ServerApplicationTest.kt
@@ -6,6 +6,7 @@ import me.rhysxia.explore.server.po.UserStatus
 import me.rhysxia.explore.server.repository.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.time.Instant
