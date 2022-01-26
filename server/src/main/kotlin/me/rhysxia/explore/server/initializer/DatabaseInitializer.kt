ec22a06fe66d63d1673276259a951504dce85052
RhysXia <xrs4433@outlook.com>
2022-01-26 13:40:25 +0800

    feat: 数据库初始化

--- server/src/main/kotlin/me/rhysxia/explore/server/initializer/DatabaseInitializer.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/initializer/DatabaseInitializer.kt
@@ -1,6 +1,9 @@
 package me.rhysxia.explore.server.initializer
 
 import io.r2dbc.spi.ConnectionFactory
+import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
@@ -10,17 +13,28 @@ import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 
 @Configuration
-class DatabaseInitializer(
-) {
+class DatabaseInitializer {
 
   @Bean
   fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
     val initializer = ConnectionFactoryInitializer()
     initializer.setConnectionFactory(connectionFactory)
-    val populator = CompositeDatabasePopulator()
-    populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
-//    populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("data.sql")))
-    initializer.setDatabasePopulator(populator)
+    val populator = CompositeDatabasePopulator(
+      ResourceDatabasePopulator(ClassPathResource("sql/schema.sql")),
+      ResourceDatabasePopulator(ClassPathResource("sql/data.sql"))
+    )
+    initializer.setDatabasePopulator {
+      mono {
+        try {
+          val result = it.createStatement("SELECT version FROM sys_info LIMIT 1").execute().awaitSingle()
+          val row = result.map { t, _ -> t }.awaitSingle()
+          println(row)
+        } catch (e: Exception) {
+          populator.populate(it).awaitSingleOrNull()
+        }
+        return@mono null
+      }
+    }
     return initializer
   }
 }
