aaaf3aca97d25d1b6995ac8f94eccf064d60e946
Rhys Xia <xrs4433@outlook.com>
2022-04-22 18:29:42 +0800

    update

--- server/src/main/kotlin/me/rhysxia/explore/server/ServerApplication.kt
+++ server/src/main/kotlin/me/rhysxia/explore/server/ServerApplication.kt
@@ -1,10 +1,6 @@
 package me.rhysxia.explore.server
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import graphql.analysis.MaxQueryComplexityInstrumentation
 import graphql.execution.instrumentation.tracing.TracingInstrumentation
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,8 +8,8 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.RedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @SpringBootApplication
@@ -26,26 +22,18 @@ class ServerApplication {
   fun maxQueryComplexityInstrumentation() = MaxQueryComplexityInstrumentation(100)
 
   @Bean
-  fun objectReactiveRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, Any> {
+  fun objectReactiveRedisTemplate(
+    factory: ReactiveRedisConnectionFactory,
+    objectMapper: ObjectMapper
+  ): ReactiveRedisTemplate<String, Any> {
     val builder = RedisSerializationContext.newSerializationContext<String, Any>()
       .key(StringRedisSerializer.UTF_8)
-      .value(RedisSerializer.json())
+      .value(GenericJackson2JsonRedisSerializer(objectMapper))
       .hashKey(StringRedisSerializer.UTF_8)
       .hashValue(StringRedisSerializer.UTF_8)
     return ReactiveRedisTemplate(
       factory, builder.build()
     )
-  }
-
-  @Bean
-  fun objectMapper(): ObjectMapper {
-    val mapper = JsonMapper.builder() // or different mapper for other format
-      .addModule(Jdk8Module())
-      .addModule(JavaTimeModule())
-      .addModule(KotlinModule.Builder().build())
-      .build();
-
-    return mapper
   }
 }
 
